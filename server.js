7d66a69483b1e6bcbe64ce5e8b9bb826d600c18b
nasirahmad3073 <nasirnawruzi@gmail.com>
2022-08-20 15:17:22 +0700

    Completed first node js server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = 8081;
+
+http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" })
+    response.write("<h1>Hello, this is from my server</h1>");
+    response.end();
+}).listen(port, () => {
+    console.log(`Node js server started on port ${port}`)
+})
